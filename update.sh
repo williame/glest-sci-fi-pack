9ecd7c36547bec4b39d5a71150dcf48d3cf734ae
William Edwards <only@joking.com>
2011-06-12 22:25:47 +0200

    lastest snapshot from Mr War

--- update.sh
+++ update.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-mkdir techs &2> /dev/null
+mkdir techs
 cd techs
 rm -rf *
 7z x ~/Downloads/sci_fi_pack.7z
 cd ..
+mkdir tilesets
+cd tilesets
+rm -rf scifi_twilight
+7x x ~/Downloads/scifi_twilight.7z
+cd ..
 detox -s lower-only -r -v techs
+detox -s lower-only -r -v tilesets
 cp LICENSE techs/sci_fi_pack
+cp LICENSE tilesets/scifi_twilight
+git add techs tilesets
+echo ===========
+git status
